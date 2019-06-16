4a11b4d136f66dae1abab4dd36985d95bf7276a1
westerxddd <sbadach@gmail.com>
2019-06-17 00:20:52 +0200

    revert imports

--- resources/views/import/page.blade.php
+++ resources/views/import/page.blade.php
@@ -10,12 +10,30 @@
     <div class="row">
         <div class="col-lg-4 col-md-6 col-sm-6 col-12">
             <div class="box box-saros">
-                @include('import.client')
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-            <div class="box box-saros">
-                @include('import.articles')
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Lp.</th>
+                            <th>Import</th>
+                            <th>Data importu</th>
+                            <th>Akcja</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($imports as $key => $import)
+                            <tr>
+                                <td>{{$key+1}}</td>
+                                <td>{{$import->id}}</td>
+                                <td>{{$import->created_at->format('d.m.Y H:i:s')}}</td>
+                                <td>
+                                    <a href="{{route('import.undo',$import->id)}}" onclick="return confirm('Jesteś pewien, że chcesz wycofać import: {{$import->id}}?')">
+                                        <button class="btn btn-sm btn-danger"><i class="fa fa-undo" aria-hidden="true"></i></button>
+                                    </a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
